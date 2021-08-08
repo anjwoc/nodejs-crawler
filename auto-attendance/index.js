30b07fb46cb5ff255e1532a03b19f9584a3692a4
anjwoc <anjwoc@naver.com>
2021-08-08 16:14:05 +0900

    1. 보안 경고페이지 무시 추가 2. 퍼페티어 시작 시 옵션 추가

--- auto-attendance/index.js
+++ auto-attendance/index.js
@@ -1,20 +1,22 @@
-const puppeteer = require('puppeteer');
-const axios = require('axios');
-const fs = require('fs');
-require('dotenv').config();
+const puppeteer = require("puppeteer");
 
-function delay(time) {
-  return new Promise((resolve, reject) => {
-    setTimeout(resolve, time);
-  });
-}
+require("dotenv").config();
 
-/**
- * tcafe 자동 출석기
- */
 const crawler = async () => {
   try {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({
+      ignoreDefaultArguments: true,
+      ignoreHTTPSErrors: true,
+      args: [
+        "--disable-web-security",
+        "--user-data-dir",
+        "--enable-usermedia-screen-capturing",
+        "--allow-http-screen-capture",
+        "--disable-infobars",
+        "--disable-features=SameSiteByDefaultCookies,CookiesWithoutSameSiteMustBeSecure",
+      ],
+      headless: false,
+    });
     const page = await browser.newPage();
     const id = process.env.ID;
     const password = process.env.PASSWORD;
@@ -23,22 +25,37 @@ const crawler = async () => {
     await page.waitForTimeout(2000);
     await page.evaluate(
       (ID, PASSWORD) => {
-        const id = document.querySelector('#ol_id');
-        const password = document.querySelector('#ol_pw');
+        const id = document.getElementById("ol_id");
+        const password = document.getElementById("ol_pw");
         const loginBtn = password.nextElementSibling;
         id.value = ID;
         password.value = PASSWORD;
         loginBtn.click();
       },
       id,
-      password,
+      password
     );
 
     await page.waitForTimeout(2000);
+
+    /**
+     * 보안 경고 페이지 무시하기
+     */
+    await page.evaluate(() => {
+      const isSecureForm = document.querySelector(".insecure-form");
+
+      if (isSecureForm) {
+        const proceedBtn = document.getElementById("proceed-button");
+        proceedBtn.click();
+      }
+    });
+
+    await page.waitForTimeout(3000);
     await page.goto(process.env.LOGIN_PAGE);
     await page.evaluate(() => {
-      document.querySelector('td img').click();
+      document.querySelector("td img").click();
     });
+    await page.waitForTimeout(3000);
 
     await page.close();
     await browser.close();
